f962544a12a2cda6f3c88c26e1f56d0cd0a0a635
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2019-05-22 10:37:42 +0800

    update path

--- sw.js
+++ sw.js
@@ -6,14 +6,14 @@ self.addEventListener('install', function (event) {
     const preCache = async () => {
         const cache = await caches.open(version);
         return cache.addAll([
-            '/resources/offline.html',
+            'resources/offline.html',
             // local
             'resources/css/index.css',
             'resources/js/index2.js',
-            '/resources/images/1.jpeg',
-            '/resources/images/2.jpeg',
-            '/resources/images/3.jpeg',
-            '/resources/images/4.jpg',
+            'resources/images/1.jpeg',
+            'resources/images/2.jpeg',
+            'resources/images/3.jpeg',
+            'resources/images/4.jpg',
             // reomote
             'https://img.alicdn.com/tfs/TB1CEMxGkvoK1RjSZFDXXXY3pXa-940-180.jpg'
         ]);
@@ -41,7 +41,7 @@ self.addEventListener('fetch', function (event) {
 
     const fetchAndUpdate = async () => {
         if (!navigator.onLine) {
-            return caches.match(new Request('/resources/offline.html'))
+            return caches.match(new Request('resources/offline.html'))
         }
 
         const promiseRes = await caches.match(event.request);
