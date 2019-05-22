090a65edb4fe11b32b89cb764c3f5629da43e8d9
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2019-05-22 10:53:41 +0800

    update path

--- resources/js/app.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/demo-serviceWorker/sw.js', { scope: '/demo-serviceWorker' })
-        .then(r => console.log('SW Registered!'))
-        .catch(console.error)
-}
